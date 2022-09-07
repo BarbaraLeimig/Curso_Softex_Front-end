3adb1a07bf0a55bcb6e4cf2928cd1a356bc41ab0
barbaraleimig <101154290+BarbaraLeimig@users.noreply.github.com>
2022-09-07 10:03:43 -0300

    Add files via upload

---
+++ 12 - calculadora_idade.py
@@ -0,0 +1,15 @@
+name = str(input('Digite o seu nome completo: '))
+info = True
+while info:
+    try:
+        birth_year = int(input('Digite o seu ano de nascimento: '))
+        if 1922 <= birth_year < 2022:
+            age = 2022 - birth_year
+            print(f'{name} completou ou completará {age} anos em 2022.')
+            break
+        else:
+            raise Exception('Erro!')
+    except Exception:
+        print('A informação digitada é inválida!')
+else:
+    info = False
