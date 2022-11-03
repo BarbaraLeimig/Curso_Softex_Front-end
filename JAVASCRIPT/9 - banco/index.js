928632883db0c39cd05eefbb7a0af79821b583d0
barbaraleimig <101154290+BarbaraLeimig@users.noreply.github.com>
2022-11-03 11:10:35 -0300

    Add files via upload

---
+++ JAVASCRIPT/9 - banco/index.js
@@ -0,0 +1,13 @@
+    var Banco = {
+        conta: 047745,
+        saldo: 0,
+        tipo_de_conta: "Conta Corrente",
+        buscar_saldo: function(){
+            return saldo
+        },
+        deposito: function(){
+
+        }
+    };
+
+    console.log(Banco.buscar_saldo)
