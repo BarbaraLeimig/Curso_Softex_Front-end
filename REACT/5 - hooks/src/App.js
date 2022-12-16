4e1dad13ab73745adcd41467f0387562d251c196
barbaraleimig <101154290+BarbaraLeimig@users.noreply.github.com>
2022-12-16 15:31:51 -0300

    Add files via upload

---
+++ REACT/5 - hooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator title="Calculadora"/>
+    </div>
+  );
+}
+
+export default App;
